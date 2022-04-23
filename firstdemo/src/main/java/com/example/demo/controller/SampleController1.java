74ad5a0f590897c767ba70e0e8dd0625dca4d7b8
jann <jjung5459@gmail.com>
2022-04-23 23:59:49 +0900

    <h1 th:text=>사용

--- firstdemo/src/main/java/com/example/demo/controller/SampleController1.java
+++ firstdemo/src/main/java/com/example/demo/controller/SampleController1.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SampleController1 {
@@ -20,6 +21,14 @@ public class SampleController1 {
 		// 뷰페이지로 데이터(model) 전달
 		
 		return "/user/userinfo";
+	}
+	
+	@GetMapping("/user/userid")
+	public String userId(@RequestParam("userid") String uid, Model model) {
+		
+		model.addAttribute("userid", uid);
+		
+		return "/user/userinfo"; // userinfo.html
 	}
 
 }
